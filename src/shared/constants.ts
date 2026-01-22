97e8e8a64125eccac3a6937ba51d09a75ef3395f
Sofiia Voitkova <sofiiavoitkova@gmail.com>
2026-01-22 20:42:22 +0100

    feat: add lorem text constant for text generation

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,3 +1,13 @@
 export const COLORS = ["#ffffffff", "#0d006cff", "#00b106ff"];
 export const BALL_COLOR = "#ff6b6b";
 export const TEXT_WORDS = ["tekst", "tekst", "tekst"];
+export const LOREM_WORDS = [
+  "lorem",
+  "ipsum",
+  "dolor",
+  "sit",
+  "amet",
+  "sed",
+  "do",
+  "elit",
+];
