5e4bfd3baf8182a138eefdc385445ea81d050ffe
Sofiia Voitkova <sofiiavoitkova@gmail.com>
2026-01-22 13:03:41 +0100

    chore: adjust import formatting

--- src/utils/createItem.ts
+++ src/utils/createItem.ts
@@ -1,5 +1,5 @@
 import type { Item } from "../shared/types";
-import { COLORS} from "../shared/constants";
+import { COLORS } from "../shared/constants";
 import { generateLabel, generateText } from "./generateItems";
 
 export function createItem(index: number): Item {
