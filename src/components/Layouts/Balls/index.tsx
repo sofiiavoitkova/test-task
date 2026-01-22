3cb2cd468a7c3ed0d4edd75fa649c85a04956e75
Sofiia Voitkova <sofiiavoitkova@gmail.com>
2026-01-22 22:29:41 +0100

    refactor: remove unused color prop and add path aliases

--- src/components/Layouts/Balls/index.tsx
+++ src/components/Layouts/Balls/index.tsx
@@ -1,17 +1,16 @@
 import { memo } from "react";
-import { BallItem } from "../../BallItem";
+import { BallItem } from "@components/BallItem";
 import styles from "./balls.module.scss";
 
 interface Props {
   count: number;
-  color: string;
 }
 
-function Balls({ count, color }: Props) {
+function Balls({ count }: Props) {
   return (
     <div className={styles.grid}>
       {Array.from({ length: count }).map((_, index) => (
-        <BallItem key={index} color={color} />
+        <BallItem key={index} />
       ))}
     </div>
   );
