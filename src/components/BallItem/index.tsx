c81b59579af271957c5b8cdcab79e8cc10b15fc9
Sofiia Voitkova <sofiiavoitkova@gmail.com>
2026-01-22 22:24:43 +0100

    refactor: simplify BallItem by removing color prop

--- src/components/BallItem/index.tsx
+++ src/components/BallItem/index.tsx
@@ -1,13 +1,5 @@
 import styles from "./ballItem.module.scss";
 
-interface Props {
-  color: string;
-}
-
-export function BallItem({ color }: Props) {
-  return (
-    <div className={styles.ball} style={{ backgroundColor: color }}>
-      {" "}
-    </div>
-  );
+export function BallItem() {
+  return <div className={styles.ball}> </div>;
 }
