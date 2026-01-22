89fba2fdf719b6001888f5861f3d6e584965329d
Sofiia Voitkova <sofiiavoitkova@gmail.com>
2026-01-22 11:41:10 +0100

    refactor: extract item creation into shared utility

---
+++ src/utils/createItem.ts
@@ -0,0 +1,12 @@
+import type { Item } from "../shared/types";
+import { COLORS} from "../shared/constants";
+import { generateLabel, generateText } from "./generateItems";
+
+export function createItem(index: number): Item {
+  return {
+    id: index + 1,
+    label: generateLabel(index, 4),
+    text: generateText(),
+    color: COLORS[Math.floor(Math.random() * COLORS.length)],
+  };
+}
